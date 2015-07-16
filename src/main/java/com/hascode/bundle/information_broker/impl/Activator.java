c472c32c58511d4979332b1213a4a0251196147b
Chris Grivas <chgrivas7@gmail.com>
2015-07-16 23:53:33 +0300

    Properties added.

--- src/main/java/com/hascode/bundle/information_broker/impl/Activator.java
+++ src/main/java/com/hascode/bundle/information_broker/impl/Activator.java
@@ -4,21 +4,24 @@ import java.util.Properties;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 import com.hascode.bundle.information_broker.api.InformationBroker;
 
 public class Activator implements BundleActivator{
 
+	private ServiceRegistration registration;
+	
 	public void start(BundleContext ctx) throws Exception {
-		ctx.registerService(
+		registration = ctx.registerService(
 				InformationBroker.class.getName(),
 				new InformationBrokerImpl(),
 				new Properties());
-		System.out.println("Hello");
+		System.out.println("InformationBroker service registered...");
 	}
 
 	public void stop(BundleContext ctx) throws Exception {
-		// TODO Auto-generated method stub
-		
+		registration.unregister();
+		System.out.println("InformationBroker service unregistered...");
 	}
 }
