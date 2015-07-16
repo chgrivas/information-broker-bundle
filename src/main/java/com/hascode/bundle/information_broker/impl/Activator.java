bb5d542e54ad1888ab503a2c4d0d2fac1d58a88a
Chris Grivas <chgrivas7@gmail.com>
2015-07-16 23:03:54 +0300

    Initial commit

---
+++ src/main/java/com/hascode/bundle/information_broker/impl/Activator.java
@@ -0,0 +1,24 @@
+package com.hascode.bundle.information_broker.impl;
+
+import java.util.Properties;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import com.hascode.bundle.information_broker.api.InformationBroker;
+
+public class Activator implements BundleActivator{
+
+	public void start(BundleContext ctx) throws Exception {
+		ctx.registerService(
+				InformationBroker.class.getName(),
+				new InformationBrokerImpl(),
+				new Properties());
+		System.out.println("Hello");
+	}
+
+	public void stop(BundleContext ctx) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
